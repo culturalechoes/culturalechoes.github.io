93b50b886d932db8b3eaf7ff9d1e33b4dfb63b9a
Marko Doda <markododa@protonmail.com>
2024-03-10 01:52:26 +0100

    Add pdfs

--- add_doc.sh
+++ add_doc.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 FILEPATH=$1
+if [ -z "$2" ]; then
+        NAME=${FILEPATH##*/}
+        NAME=${NAME%%.*}
+else
 NAME=$2
+fi
+
 cat <<EOF
-        <div class="col-lg-4 col-md-4 col-sm-4 portfolio-item">
+        <div class="col-lg-4 col-md-4 col-sm-6 portfolio-item">
           <div class="card d-block border-none">
 
             <div class="row">
-              <div class ="col-lg-4 col-md-4 col-sm-4 col-4 display-text-docs">
+              <div class ="col-lg-2 col-md-3 col-sm-2 col-2 display-text-docs">
                 <img class="card-img-top " src="./img/doc - tmb/google-docs.png" alt="">
               </div>
               <div class ="col-lg-10 col-md-9 col-sm-10 display-text-docs">
@@ -17,3 +23,4 @@ cat <<EOF
           </div>
         </div>
 EOF
+#find  src/pdf/ -type f -mtime -1|sort -n -k 4 |xargs -I {} ./add_doc.sh {}|xclip
